e8cc72aa95f79bdda47d43e54ef90600fd6032dc
Olav Møyner <olav.moyner@gmail.com>
2025-01-30 15:55:44 +0100

    Improvements to plots

--- CSP11Visualizer/src/dense.jl
+++ CSP11Visualizer/src/dense.jl
@@ -66,18 +66,31 @@ function make_movie(results, k, t = k; filename = "sg.mp4")
     return filename
 end
 
-function plot_snapshot(result, k, t = k)
+function plot_snapshot(result, k)
+    name = "$k"
+    clims, t, zero_to_nan = key_info(name, result["case"])
     # GLMakie.activate!()
-    r = result["$k"]
+    r = result[name]
     x = result["x"]
     z = result["z"]
     fig = Figure(size = (1200, 600), backgroundcolor = :transparent)
-    D = vec(r)
+    D = copy(vec(r))
+    if zero_to_nan
+        D[D .== 0] .= NaN
+    end
     failure = all(isnan, D)
-    ax = Axis(fig[1, 1], title = t)
+    ax = Axis(fig[1, 1], title = t, ygridvisible = false, xgridvisible = false)
     if !failure
+        if isnothing(clims)
+            arg = NamedTuple()
+        else
+            arg = (colorrange = clims, )
+        end
         try
-            plt = heatmap!(ax, vec(x), vec(z), D, colormap = default_colormap())
+            plt = heatmap!(ax, vec(x), vec(z), D;
+                colormap = default_colormap(),
+                arg...
+            )
             Colorbar(fig[1, 2], plt)
         catch excpt
             @error "Failed to plot $k" excpt
@@ -85,7 +98,6 @@ function plot_snapshot(result, k, t = k)
         end
     end
     if failure
-        hidedecorations!(ax)
         text!(ax, 0.5, 0.5, text = "Data missing / plot failure.", fontsize = 50, align = (:center, :baseline))
     end
     return fig
@@ -191,7 +203,7 @@ function parse_dense_data(group, result, year_or_h, case = "b", path = default_d
         return zi*(Lx*Ly) + yi*Lx + xi
     end
     ix = sort(eachindex(z), by = sortfunction)
-    out = Dict{String, AbstractArray{Float64}}()
+    out = Dict{String, Union{AbstractArray{Float64}, String}}()
     for name in names(df)
         val = df[:, name]
         if eltype(val) != Float64 || eltype(val) != Int64
@@ -205,5 +217,49 @@ function parse_dense_data(group, result, year_or_h, case = "b", path = default_d
         end
         out[name] = val
     end
+    out["case"] = case
     return out
+end
+
+function key_info(var::String, case::String)
+    yscale = nothing
+    label = ""
+    zero_to_nan = false
+    if case == "b"
+        # Commented numbers are taken from OPM 1
+        if var == "T"
+            yscale = (0.0, 75.0)
+            label = "Temperature (°C)"
+        elseif var == "pw"
+            yscale = (3e7, 4.5e7) # 3.23e7 -> 4.47e7
+            label =  "Pressure (Pascal)"
+        elseif var == "co2mass"
+            yscale = (0.0, 30000.0) #  (0.0, 28550.0)
+            label = "Total mass of CO₂ (kg)"
+            zero_to_nan = true
+        elseif var == "X_co2"
+            yscale = (0.0, 0.1)
+            label = "CO₂ mass fraction in liquid"
+        elseif var == "Y_h2o"
+            yscale = (0.0, 0.005)
+            label = "H₂O mass fraction in vapor"
+        elseif var == "sg"
+            yscale = (0.0, 1.0)
+            label = "Gas saturation"
+        elseif var == "denw"
+            yscale = (990, 1050.0)
+            label = "Water density (kg/m³)"
+            zero_to_nan = true
+        elseif var == "deng"
+            yscale = (900, 1060.0)
+            label = "Gas density (kg/m³)"
+            zero_to_nan = true
+        end
+    else
+
+    end
+    if isnothing(yscale)
+        @warn "No scaling for $var for case $case"
+    end
+    return (yscale, label, zero_to_nan)
 end
--- scripts/dense_b_template.jl
+++ scripts/dense_b_template.jl
@@ -1,5 +1,6 @@
-# # HEADER for case B
+# # Case B: HEADER
 groupname = "sintef" # hide
+groupname = "opm"
 resultid = 1 # hide
 using CSP11Visualizer, GLMakie, CairoMakie # hide
 CairoMakie.activate!() # hide
@@ -14,60 +15,62 @@ end_of_migration = findfirst(isequal(1000), steps) # hide
 @assert !isnothing(end_of_migration); # hide
 # ## Thermodynamic state variables
 # ### Total mass of CO₂
-
+# The total mass of CO₂ in kilograms per cell is shown below. This is the
+# absolute mass of CO₂ and indicates where the CO₂ is located in the domain.
 # #### End of injection
-CSP11Visualizer.plot_snapshot(results[end_of_injection], :co2mass, "Total mass of CO₂ (kg)") # hide
+CSP11Visualizer.plot_snapshot(results[end_of_injection], :co2mass) # hide
 # #### End of migration
-CSP11Visualizer.plot_snapshot(results[end_of_migration], :co2mass, "Total mass of CO₂ (kg)") # hide
+CSP11Visualizer.plot_snapshot(results[end_of_migration], :co2mass) # hide
 
 # ### Pressure
-
+# The reported pressure in Pascal is shown below. This is the pressure of the
+# water phase.
 # #### End of injection
-CSP11Visualizer.plot_snapshot(results[end_of_injection], :pw, "Pressure (Pascal)") # hide
+CSP11Visualizer.plot_snapshot(results[end_of_injection], :pw,) # hide
 # #### End of migration
-CSP11Visualizer.plot_snapshot(results[end_of_migration], :pw, "Pressure (Pascal)") # hide
+CSP11Visualizer.plot_snapshot(results[end_of_migration], :pw) # hide
 
 
 # ### Temperature
 
 # #### End of injection
-CSP11Visualizer.plot_snapshot(results[end_of_injection], :T, "Temperature (°C)") # hide
+CSP11Visualizer.plot_snapshot(results[end_of_injection], :T) # hide
 # #### End of migration
-CSP11Visualizer.plot_snapshot(results[end_of_migration], :T, "Temperature (°C)") # hide
+CSP11Visualizer.plot_snapshot(results[end_of_migration], :T) # hide
 
 # ## Additional reporting variables
 
 # ## Gas saturation
 
 # ### End of injection
-CSP11Visualizer.plot_snapshot(results[end_of_injection], :deng, "Gas saturation") # hide
+CSP11Visualizer.plot_snapshot(results[end_of_injection], :sg) # hide
 # ### End of migration
-CSP11Visualizer.plot_snapshot(results[end_of_migration], :deng, "Gas saturation") # hide
+CSP11Visualizer.plot_snapshot(results[end_of_migration], :sg) # hide
 
 # ## CO₂ mass fraction in liquid
 
 # ### End of injection
-CSP11Visualizer.plot_snapshot(results[end_of_injection], :X_co2, "CO₂ mass fraction in liquid") # hide
+CSP11Visualizer.plot_snapshot(results[end_of_injection], :X_co2) # hide
 # ### End of migration
-CSP11Visualizer.plot_snapshot(results[end_of_migration], :X_co2, "CO₂ mass fraction in liquid") # hide
+CSP11Visualizer.plot_snapshot(results[end_of_migration], :X_co2) # hide
 
 # ## H₂O mass fraction in vapor
 
 # ### End of injection
-CSP11Visualizer.plot_snapshot(results[end_of_injection], :Y_h2o, "H₂O mass fraction in vapor") # hide
+CSP11Visualizer.plot_snapshot(results[end_of_injection], :Y_h2o) # hide
 # ### End of migration
-CSP11Visualizer.plot_snapshot(results[end_of_migration], :Y_h2o, "H₂O mass fraction in vapor") # hide
+CSP11Visualizer.plot_snapshot(results[end_of_migration], :Y_h2o) # hide
 
 # ## Gas density
 
 # ### End of injection
-CSP11Visualizer.plot_snapshot(results[end_of_injection], :deng, "Gas density (kg/m³)") # hide
+CSP11Visualizer.plot_snapshot(results[end_of_injection], :deng) # hide
 # ### End of migration
-CSP11Visualizer.plot_snapshot(results[end_of_migration], :deng, "Gas density (kg/m³)") # hide
+CSP11Visualizer.plot_snapshot(results[end_of_migration], :deng) # hide
 
 # ## Water density
 
 # ### End of injection
-CSP11Visualizer.plot_snapshot(results[end_of_injection], :denw, "Water density (kg/m³)") # hide
+CSP11Visualizer.plot_snapshot(results[end_of_injection], :denw) # hide
 # ### End of migration
-CSP11Visualizer.plot_snapshot(results[end_of_migration], :denw, "Water density (kg/m³)") # hide
+CSP11Visualizer.plot_snapshot(results[end_of_migration], :denw) # hide
